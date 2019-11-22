1e7775fd0d4bf377b1bef568e8951237cb775b61
dbstocker <dstocker@notesettersinc.com>
2019-11-22 12:57:51 -0500

    pig game finished, in need of dry-ing

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,51 +9,57 @@ GAME RULES:
 
 */
 
-let scores, roundScore, activePlayer, diceDOM;
-
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
+let scores, roundScore, activePlayer, diceDOM, gamePlaying;
 diceDOM = document.querySelector(".dice");
 
-document.querySelector(".dice").style.display = "none";
-
-document.getElementById("score-0").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-1").textContent = "0";
+init();
 
 document.querySelector(".btn-roll").addEventListener("click", () => {
-  // 1. random number 1-6
-  let dice = Math.floor(Math.random() * 6) + 1;
+  if(gameIsPlaying) {
+    // 1. random number 1-6
+    let dice = Math.floor(Math.random() * 6) + 1;
 
-  // 2. display result
-  diceDOM.style.display = "block";
-  diceDOM.src = `dice-${dice}.png`;
-  // document.querySelector(`#current-${activePlayer}`).textContent = dice;
+    // 2. display result
+    diceDOM.style.display = "block";
+    diceDOM.src = `dice-${dice}.png`;
+    // document.querySelector(`#current-${activePlayer}`).textContent = dice;
 
-  // 3. update round score if roll != 1
-  if (dice !== 1) {
-    // add score
-    roundScore += dice;
-    document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
-  } else {
-    // next player
-    nextPlayer();
-    
+    // 3. update round score if roll != 1
+    if (dice !== 1) {
+      // add score
+      roundScore += dice;
+      document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+    } else {
+      // next player
+      nextPlayer();
+      
+    }
   }
 });
 
 document.querySelector(".btn-hold").addEventListener("click", () => {
-  // add current score to global score
-  scores[activePlayer] += roundScore;
-  // update the UI
-  document.querySelector(`#score-${activePlayer}`).textContent =
-    scores[activePlayer];
-  nextPlayer();
-  // check if activeplayer wins game
+  if(gameIsPlaying) {
+    // add current score to global score
+    scores[activePlayer] += roundScore;
+    // update the UI
+    document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer];
+    // document.querySelector('.btn-hold').setAttribute('disabled', 'true');
+
+    // check if activeplayer wins game
+    if (scores[activePlayer] >= 10) {
+      document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!';
+      diceDOM.style.display = 'none';
+      document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
+      document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
+      gameIsPlaying = false;
+    } else {
+      // next player
+      nextPlayer();
+    }
+  }
 });
+
+document.querySelector('.btn-new').addEventListener('click', init);
 
 function nextPlayer() {
   activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
@@ -65,6 +71,26 @@ function nextPlayer() {
   document.querySelector(".player-1-panel").classList.toggle("active");
 
   diceDOM.style.display = "none";
+}
+
+function init() {
+  gameIsPlaying = true;
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+
+  diceDOM.style.display = "none";
+
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+
+  document.querySelector('.player-0-panel').classList.add('active');
 }
 // document.querySelector(`#current-${activePlayer}`).innerHTML = `<em>${dice}</em>`;
 
