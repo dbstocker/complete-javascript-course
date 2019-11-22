39dd51e7bdf2841c032eb092f32ca27ead26dd68
dbstocker <dstocker@notesettersinc.com>
2019-11-22 06:25:03 -0500

    btn-roll logic first run

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,14 +9,49 @@ GAME RULES:
 
 */
 
-let scores, roundScore, activePlayer, dice;
+let scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector(`#current-${activePlayer}`).textContent = dice;
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', () => {
+  // 1. random number 1-6
+  let dice = Math.floor(Math.random() * 6) + 1;
+  
+  // 2. display result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = `dice-${dice}.png`;
+  // document.querySelector(`#current-${activePlayer}`).textContent = dice;
+  
+  // 3. update round score if roll != 1
+  if (dice !== 1) {
+    // add score
+    roundScore += dice;
+    document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+  } else {
+    // next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    diceDOM.style.display = 'none';
+  }
+});
+
 // document.querySelector(`#current-${activePlayer}`).innerHTML = `<em>${dice}</em>`;
 
+// var x = document.querySelector('#score-0').textContent;
